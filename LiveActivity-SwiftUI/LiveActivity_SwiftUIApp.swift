9dec6e2403faa2781ba4ceea0aff7f113892c815
Francisco Juan <juansoussev@gmail.com>
2025-07-21 11:30:16 -0300

    Initial Commit

---
+++ LiveActivity-SwiftUI/LiveActivity_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivity_SwiftUIApp.swift
+//  LiveActivity-SwiftUI
+//
+//  Created by Francisco Juan on 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivity_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
